5499ece0feb8e7295249ca5534afd04a6662d056
임또이 <sh663746@gmail.com>
2022-01-12 15:41:39 +0900

    카드구매하기

--- src/DynamicProgramming/BOJ_11052.java
+++ src/DynamicProgramming/BOJ_11052.java
@@ -3,38 +3,31 @@ package DynamicProgramming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_11052 {
-    static String str;
+    static int n;
     static BufferedReader br;
-    static long[] dp;
+    static StringTokenizer st;
+    static int[] dp, price;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
-        str = br.readLine();
-        int size = str.length();
-        dp = new long[size + 1];
-        dp[0] = dp[1] = 1;
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        price = new int[n + 1];
+        st = new StringTokenizer(br.readLine(), " ");
 
-        if (str.charAt(0) == '0' || str.charAt(size - 1) == '0' && str.charAt(size - 2) != '1' && str.charAt(size - 2) != '2') {
-            System.out.println("0");
-        } else {
-            for (int i = 2; i <= size; i++) {
-                //마지막 한자리 수 -> 한글자일 경우
-                int temp = Integer.parseInt(str.substring(i - 1, i));
-                if (temp <= 9 && temp >= 1) {
-                    dp[i] = dp[i - 1];
-                }
-
-                temp += Integer.parseInt(str.substring(i - 2, i - 1)) * 10; // 10의 자리수
-                if (temp <= 26 && temp >= 10) {
-                    dp[i] += dp[i - 2];
-                }
-                dp[i] %= 1000000;
-            }
-            System.out.println(dp[size]);
+        for (int i = 1; i <= n; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
         }
 
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= i; k++) {
+                dp[i] = Math.max(dp[i], dp[i - k] + price[k]);
+            }
+        }
+        System.out.println(dp[n]);
     }
 
 }
