a2ecc844ad0b7c82018387d776281e2a232e6828
DESKTOP-P2FGL4R\KPC-N-0030 <sh663746@gmail.com>
2022-06-22 13:43:09 +0900

    BOJ_11725 solved

--- src/Graph/DFS/BOJ_11725.java
+++ src/Graph/DFS/BOJ_11725.java
@@ -10,19 +10,21 @@ public class BOJ_11725 {
     static int n, m;
     static ArrayList<Integer>[] a;
     static boolean[] visit;
+    static int[] parents;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
         m = n - 1;
+        parents = new int[n + 1];
         StringTokenizer st;
         //데이터를 담을 인접리스트
         a = new ArrayList[n + 1];
         //방문기록
         visit = new boolean[n + 1];
         //노드의 개수만큼 인접 리스트 초기화
-        for(int i = 1; i < n + 1; i++) {
+        for (int i = 1; i < n + 1; i++) {
             a[i] = new ArrayList<Integer>();
         }
         //엣지의 개수만큼 데이터 삽입
@@ -35,22 +37,22 @@ public class BOJ_11725 {
             a[e].add(s);
         }
 
-        for(int i = 1; i < n+1; i++) {
-            //1부터 시작해서 depth가 1일때까지만
-            DFS(i,1);
+        DFS(1);
 
+        for (int i = 2; i < n + 1; i++) {
+            System.out.println(parents[i]);
         }
 
     }
 
-    private static void DFS(int v, int depth) {
-        if(depth == 1) return;
+    private static void DFS(int v) {
+
         visit[v] = true;
-        for(int i : a[v]) {
-            if(!visit[i]){
-                DFS(i, depth + 1);
+        for (int i : a[v]) {
+            if (!visit[i]) {
+                parents[i] = v;
+                DFS(i);
             }
         }
-        visit[v] = false;
     }
 }
