5d8a8a739bf21d7727f47e236d8dc60a8f50509d
임또이 <sh663746@gmail.com>
2022-10-18 23:25:04 +0900

    20221018

---
+++ src/Programmers/level2/점프와순간이동.java
@@ -0,0 +1,12 @@
+package Programmers.level2;
+
+public class 점프와순간이동 {
+    public int solution(int n) {
+        int ans = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return ans;
+    }
+}
