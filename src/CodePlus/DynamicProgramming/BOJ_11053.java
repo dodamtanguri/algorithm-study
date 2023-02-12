5884b5b7d07c0e99e4a3d0abac112eed1dad1f17
임또이 <sh663746@gmail.com>
2023-02-12 23:00:17 +0900

    02.12 백준_가장_긴_증가하는_부분수열

---
+++ src/CodePlus/DynamicProgramming/BOJ_11053.java
@@ -0,0 +1,37 @@
+package CodePlus.DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11053 {
+    static int[] a;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 수열 A의 크기
+        a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] d = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i] && d[i] < d[j] + 1) {
+                    d[i] = d[j] + 1;
+                }
+            }
+        }
+
+        int ans = d[0];
+        for (int i = 0; i < n; i++) {
+            if (ans < d[i]) {
+                ans = d[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
