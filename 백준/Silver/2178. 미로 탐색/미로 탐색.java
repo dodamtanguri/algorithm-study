75c748371db16495c51703eb3e1ee9375b658411
Sophie <67816339+dodamtanguri@users.noreply.github.com>
2023-04-26 20:29:39 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12244 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] maze = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = row.charAt(j) - '0';
+            }
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M || maze[nx][ny] == 0) {
+                    continue;
+                }
+
+                if (maze[nx][ny] == 1) {
+                    queue.offer(new int[]{nx, ny});
+                    maze[nx][ny] = maze[x][y] + 1;
+                }
+            }
+        }
+
+        System.out.println(maze[N - 1][M - 1]);
+    }
+}
