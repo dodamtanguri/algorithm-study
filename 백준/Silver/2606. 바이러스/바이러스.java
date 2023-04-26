644b1ade2724119a13005b8de4eee81221414bd0
Sophie <67816339+dodamtanguri@users.noreply.github.com>
2023-04-26 20:42:19 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11776 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,66 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m, ans;
+    static ArrayList<Integer>[] a;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        a = new ArrayList[n + 1];
+        visit = new boolean[n + 1];
+        //노드의 개수만큼 인접리스트 초기화
+        for (int i = 1; i < n + 1; i++) {
+            a[i] = new ArrayList<Integer>();
+        }
+
+        //엣지의 개수만큼 데이터 삽입
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            a[s].add(e);
+            a[e].add(s);
+        }
+
+//        DFS(1);
+        BFS(1);
+
+
+
+    }
+
+    private static void BFS(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visit[v] = true;
+        while (!queue.isEmpty()) {
+            v = queue.poll();
+            for(int i : a[v]) {
+                if(visit[i]) continue;
+                queue.add(i);
+                visit[i] = true;
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+
+//    static void DFS(int i) {
+//        visit[i] = true;
+//        for (int k : a[i]) {
+//            if (!visit[k]) {
+//                ans++;
+//                DFS(k);
+//            }
+//        }
+//
+//    }
+}
