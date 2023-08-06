1f94f26b2e2630556b4e78568fb559d74af5d7a3
Sophie <67816339+dodamtanguri@users.noreply.github.com>
2023-08-06 21:36:15 +0900

    [Silver II] Title: 겉넓이 구하기, Time: 104 ms, Memory: 12688 KB -BaekjoonHub

--- 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
+++ 백준/Silver/16931. 겉넓이 구하기/겉넓이 구하기.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
+        
         int[][] grid = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -16,37 +16,37 @@ public class Main {
                 grid[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
+        
         System.out.println(getSurfaceArea(grid));
-    }
 
+    }
     public static int getSurfaceArea(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        int totalArea = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                // 현재 셀의 블록들에 대한 표면적
-                totalArea += 6 * grid[i][j];
-
-                // 현재 셀에 쌓인 블록들 사이의 숨겨진 표면
-                if (grid[i][j] > 1) {
-                    totalArea -= 2 * (grid[i][j] - 1);
+        int total = 0;
+        
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                //현재셀 
+                total += 6 * grid[i][j];
+                //쌓인 블록들 사이에 숨겨진거 표면 계산
+                
+                if(grid[i][j]>1) {
+                    total -= 2 * (grid[i][j]-1);
                 }
-
-                // 왼쪽 셀로부터 숨겨진 표면
-                if (i > 0) {
-                    totalArea -= 2 * Math.min(grid[i][j], grid[i-1][j]);
+                //왼쪽 셀에서 숨겨진거 찾는거 
+                // Math.min(grid[i][j], grid[i-1][j]); >> 더 낮은 블럭의 표면을 찾는게. 
+                if(i > 0) {
+                    total -= 2 * Math.min(grid[i][j], grid[i-1][j]);
                 }
-
-                // 위쪽 셀로부터 숨겨진 표면
-                if (j > 0) {
-                    totalArea -= 2 * Math.min(grid[i][j], grid[i][j-1]);
+                //위쪽 셀에서 숨겨진 표면 찾는거 
+                if(j > 0) {
+                    total -= 2 * Math.min(grid[i][j], grid[i][j-1]);
                 }
+               
             }
         }
-
-        return totalArea;
+        return total;
+        
     }
 }
